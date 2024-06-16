5900e2573a0f22e1ea421e4c5e7152aa340a648e
deshratnadevendra <devendradeshratna@gmal.com>
2024-06-16 13:05:26 +0530

    Config changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5173/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function register(user) {
   const res = await fetch(`${API_URL}/register`, {
