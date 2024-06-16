ef3da3516fe581bd327367c21c776d3ab1b6efe8
deshratnadevendra <devendradeshratna@gmal.com>
2024-06-16 13:26:49 +0530

    Config changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
-  plugins: [svelte()],
-  // Add this to ensure environment variables are loaded
-  define: {
-    'process.env': process.env
-  }
+  plugins: [sveltekit()],
 });
